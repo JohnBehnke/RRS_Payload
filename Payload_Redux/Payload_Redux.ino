2ca3f5b4fcf26f921dc11a8d8ce36f183455f955
John Behnke <behnkej@me.com>
2014-09-12 13:15:17 -0400

    Formatting

--- Payload_Redux/Payload_Redux.ino
+++ Payload_Redux/Payload_Redux.ino
@@ -1,23 +1,56 @@
 //Payload Redux
 
-
-
+// ================================================================
+// ===               BMP085 Include Statements                  ===
+// ================================================================
 #include <Wire.h>
 
+// ================================================================
+// ===                 MPU6050 Include Statements               ===
+// ================================================================
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+//#define OUTPUT_READABLE_QUATERNION
+//#define OUTPUT_READABLE_EULER
+#define OUTPUT_READABLE_YAWPITCHROLL
+//#define OUTPUT_READABLE_REALACCEL
+//#define OUTPUT_READABLE_WORLDACCEL
+
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 
-//Pin Setup
-
-//SHT15
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+// ================================================================
+// ===                        SHT15 Setup                       ===
+// ================================================================
 int clockPin = 11;  // pin used for clock
 int dataPin  = 10;  // pin used for data
-
-//BMP085
-
-
-
-
-//SHT15
 int temperatureCommand  = 3;  // command used to read temperature
 int humidityCommand = 5;  // command used to read humidity
 int ack;  // track acknowledgment for errors
@@ -25,9 +58,9 @@ int val;
 float temperature;
 float humidity;
 
-
-
-//BMP085
+// ================================================================
+// ===                        BMP085 Setup                      ===
+// ================================================================
 #define BMP085_ADDRESS 0x77  // I2C address of BMP085
 const unsigned char OSS = 0;  // Oversampling Setting
 // Calibration values
@@ -48,22 +81,78 @@ long pressure;
 // Use these for altitude conversions
 const float p0 = 101325;     // Pressure at sea level (Pa)
 float altitude;
-
+///////////////////////////////////
 void setup() {
-    Serial.begin(9600); // open serial at 9600 bps
+    Serial.begin(115200); // open serial at 9600 bps
 
-    //BMP085
+    // ================================================================
+    // ===                            BMP085                        ===
+    // ================================================================
     Wire.begin();
     bmp085Calibration();
 
+    // ================================================================
+    // ===                           MPU6050                        ===
+    // ================================================================
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+#endif
+    mpu.initialize();
+    devStatus = mpu.dmpInitialize();
+
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+    mpu.setDMPEnabled(true);
+
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(0, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
 }
+
+
+// ================================================================
+// ===                        ------------                      ===
+// ================================================================
 
 void loop() {
 
+    // ================================================================
+    // ===                       SHT15 Main Code                    ===
+    // ================================================================
 
 
-    //SHT15
-    //////////////////////////////////////////////////////////////////
+
     // read the temperature
     sendCommandSHT(temperatureCommand, dataPin, clockPin);
     waitForResultSHT(dataPin);
@@ -84,12 +173,10 @@ void loop() {
     Serial.print("        ");
     Serial.println(humidity);
     // wait for 1 second for next reading
-    //////////////////////////////////////////////////////////////////
 
-    //BMP085
-    //////////////////////////////////////////////////////////////////
-
-
+    // ================================================================
+    // ===                     BMP085 Main Code                     ===
+    // ================================================================
     pressure = bmp085GetPressure(bmp085ReadUP());
     altitude = (float)44330 * (1 - pow(((float) pressure / p0), 0.190295));
 
@@ -103,13 +190,126 @@ void loop() {
     Serial.println("________________");
     //delay(1000);
 
+    // ================================================================
+    // ===                    MPU6050 Main LOOP                     ===
+    // ================================================================
+
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        //reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+        // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+#ifdef OUTPUT_READABLE_QUATERNION
+        // display quaternion values in easy matrix form: w x y z
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        Serial.print("quat\t");
+        Serial.print(q.w);
+        Serial.print("\t");
+        Serial.print(q.x);
+        Serial.print("\t");
+        Serial.print(q.y);
+        Serial.print("\t");
+        Serial.println(q.z);
+#endif
+
+#ifdef OUTPUT_READABLE_EULER
+        // display Euler angles in degrees
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetEuler(euler, &q);
+        Serial.print("euler\t");
+        Serial.print(euler[0] * 180 / M_PI);
+        Serial.print("\t");
+        Serial.print(euler[1] * 180 / M_PI);
+        Serial.print("\t");
+        Serial.println(euler[2] * 180 / M_PI);
+#endif
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+        // display Euler angles in degrees
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+        Serial.print("ypr\t");
+        Serial.print(ypr[0] * 180 / M_PI);
+        Serial.print("\t");
+        Serial.print(ypr[1] * 180 / M_PI);
+        Serial.print("\t");
+        Serial.println(ypr[2] * 180 / M_PI);
+#endif
+
+#ifdef OUTPUT_READABLE_REALACCEL
+        // display real acceleration, adjusted to remove gravity
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetAccel(&aa, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+        Serial.print("areal\t");
+        Serial.print(aaReal.x);
+        Serial.print("\t");
+        Serial.print(aaReal.y);
+        Serial.print("\t");
+        Serial.println(aaReal.z);
+#endif
+
+#ifdef OUTPUT_READABLE_WORLDACCEL
+        // display initial world-frame acceleration, adjusted to remove gravity
+        // and rotated based on known orientation from quaternion
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetAccel(&aa, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+        mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+        Serial.print("aworld\t");
+        Serial.print(aaWorld.x);
+        Serial.print("\t");
+        Serial.print(aaWorld.y);
+        Serial.print("\t");
+        Serial.println(aaWorld.z);
+#endif
+
+#ifdef OUTPUT_TEAPOT
+        // display quaternion values in InvenSense Teapot demo format:
+        teapotPacket[2] = fifoBuffer[0];
+        teapotPacket[3] = fifoBuffer[1];
+        teapotPacket[4] = fifoBuffer[4];
+        teapotPacket[5] = fifoBuffer[5];
+        teapotPacket[6] = fifoBuffer[8];
+        teapotPacket[7] = fifoBuffer[9];
+        teapotPacket[8] = fifoBuffer[12];
+        teapotPacket[9] = fifoBuffer[13];
+        Serial.write(teapotPacket, 14);
+        teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
+#endif
+
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+    }
 }
 
 
-
-//SHT15 CODE
-///////////////////////////////////////////////////
-///////////////////////////////////////////////////
+// ================================================================
+// ===                      SHT15 Functions                     ===
+// ================================================================
 
 // commands for reading/sending data to a SHTx sensor
 int shiftIn(int dataPin, int clockPin, int numBits) {
@@ -200,14 +400,10 @@ void skipCrcSHT(int dataPin, int clockPin) {
     digitalWrite(clockPin, HIGH);
     digitalWrite(clockPin, LOW);
 }
-///////////////////////////////////////////////////
-///////////////////////////////////////////////////
 
-
-
-//BMP085 Code
-///////////////////////////////////////////////////
-///////////////////////////////////////////////////
+// ================================================================
+// ===                     BMP085 Functions                     ===
+// ================================================================
 int bmp085ReadInt(unsigned char address) {
     unsigned char msb, lsb;
 
@@ -354,14 +550,11 @@ char bmp085Read(unsigned char address) {
     return Wire.read();
 }
 
-// Read 2 bytes from the BMP085
-// First byte will be from 'address'
-// Second byte will be from 'address'+1
+// ================================================================
+// ===                     MPU6050 Functions                    ===
+// ================================================================
 
-
-
-///////////////////////////////////////////////////
-///////////////////////////////////////////////////
-
-
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
 
