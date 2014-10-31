bba673e0f2421f38f5e57d62f00400008f614694
John Behnke <behnkej@me.com>
2014-10-31 13:32:55 -0400

    Deleted old prints. There will be a new format

--- payload/payload.ino
+++ payload/payload.ino
@@ -14,12 +14,6 @@ Serial.print(pressure, DEC);
 
 Serial.print("|");
 Serial.print(humidity);
-Serial.print("|");
-print_float(flat, TinyGPS::GPS_INVALID_F_ANGLE, 10, 6);
-Serial.print("|");
-print_float(flon, TinyGPS::GPS_INVALID_F_ANGLE, 11, 6);
-Serial.print("|");
-print_float(gps.f_altitude(), TinyGPS::GPS_INVALID_F_ALTITUDE, 7, 2);
 
 
 
