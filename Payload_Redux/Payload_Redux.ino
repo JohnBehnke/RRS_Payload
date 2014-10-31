86247392d77eec7f3699f9d57f853a04e6748fb2
John Behnke <behnkej@me.com>
2014-10-31 13:39:35 -0400

    Weird I2C setup gone

--- Payload_Redux/Payload_Redux.ino
+++ Payload_Redux/Payload_Redux.ino
@@ -1,143 +1,40 @@
-//Payload Redux
+/
+devStatus = mpu.dmpInitialize();
 
-// ================================================================
-// ===               BMP085 Include Statements                  ===
-// ================================================================
-#include <Wire.h>
+mpu.setXGyroOffset(220);
+mpu.setYGyroOffset(76);
+mpu.setZGyroOffset(-85);
+mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+mpu.setDMPEnabled(true);
 
-// ================================================================
-// ===                 MPU6050 Include Statements               ===
-// ================================================================
-#include "I2Cdev.h"
-
-#include "MPU6050_6Axis_MotionApps20.h"
-
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
-#endif
-
-MPU6050 mpu;
-
-//#define OUTPUT_READABLE_QUATERNION
-//#define OUTPUT_READABLE_EULER
-#define OUTPUT_READABLE_YAWPITCHROLL
-//#define OUTPUT_READABLE_REALACCEL
-//#define OUTPUT_READABLE_WORLDACCEL
-
-#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
-bool blinkState = false;
-
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-// ================================================================
-// ===                        SHT15 Setup                       ===
-// ================================================================
-int clockPin = 11;  // pin used for clock
-int dataPin  = 10;  // pin used for data
-int temperatureCommand  = 3;  // command used to read temperature
-int humidityCommand = 5;  // command used to read humidity
-int ack;  // track acknowledgment for errors
-int val;
-float temperature;
-float humidity;
-
-// ================================================================
-// ===                        BMP085 Setup                      ===
-// ================================================================
-#define BMP085_ADDRESS 0x77  // I2C address of BMP085
-const unsigned char OSS = 0;  // Oversampling Setting
-// Calibration values
-int ac1;
-int ac2;
-int ac3;
-unsigned int ac4;
-unsigned int ac5;
-unsigned int ac6;
-int b1;
-int b2;
-int mb;
-int mc;
-int md;
-long b5;
-
-long pressure;
-// Use these for altitude conversions
-const float p0 = 101325;     // Pressure at sea level (Pa)
-float altitude;
-///////////////////////////////////
-void setup() {
-    Serial.begin(115200); // open serial at 9600 bps
-
-    // ================================================================
-    // ===                            BMP085                        ===
-    // ================================================================
-    Wire.begin();
-    bmp085Calibration();
-
-    // ================================================================
-    // ===                           MPU6050                        ===
-    // ================================================================
-
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    Wire.begin();
-    TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-    Fastwire::setup(400, true);
-#endif
-    mpu.initialize();
-    devStatus = mpu.dmpInitialize();
-
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+if (devStatus == 0) {
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
     mpu.setDMPEnabled(true);
 
-    if (devStatus == 0) {
-        // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
+    // enable Arduino interrupt detection
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    attachInterrupt(0, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
 
-        // enable Arduino interrupt detection
-        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-        attachInterrupt(0, dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
 
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+} else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+}
 
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
-    }
-
-    // configure LED for output
-    pinMode(LED_PIN, OUTPUT);
+// configure LED for output
+pinMode(LED_PIN, OUTPUT);
 }
 
 
