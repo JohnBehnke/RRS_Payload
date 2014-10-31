b22c9a7231de62e6d0e1bbfd0ec89f7be9d2af77
John Behnke <behnkej@me.com>
2014-10-31 13:33:37 -0400

    Old code gone

--- payload/payload.ino
+++ payload/payload.ino
@@ -8,13 +8,6 @@ Serial.print("OK|");
 //Serial.print("|");
 Serial.print(temperature);
 //  Serial.println(" deg C");
-Serial.print("|");
-Serial.print(pressure, DEC);
-//  Serial.println(" Pa");
-
-Serial.print("|");
-Serial.print(humidity);
-
 
 
 //smartdelay(100);//just here to slow down the serial output - Easier to read
