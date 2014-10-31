d34ef5976e9dcbec43e77e3516744ad211f898ff
John Behnke <behnkej@me.com>
2014-10-31 13:32:35 -0400

    Deleted old print statements

--- payload/payload.ino
+++ payload/payload.ino
@@ -20,10 +20,7 @@ Serial.print("|");
 print_float(flon, TinyGPS::GPS_INVALID_F_ANGLE, 11, 6);
 Serial.print("|");
 print_float(gps.f_altitude(), TinyGPS::GPS_INVALID_F_ALTITUDE, 7, 2);
-Serial.print("|");
-Serial.print(time_since_start)
-Serial.print("|OK");
-Serial.println();
+
 
 
 //smartdelay(100);//just here to slow down the serial output - Easier to read
