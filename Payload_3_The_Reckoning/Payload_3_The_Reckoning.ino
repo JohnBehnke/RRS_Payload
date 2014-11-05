876ce133027d00ddbf65e8e08fdf88d5bba65242
Brian Kelley <bkelley620@gmail.com>
2014-11-04 20:47:56 -0500

    Added support for the SD card logging

--- Payload_3_The_Reckoning/Payload_3_The_Reckoning.ino
+++ Payload_3_The_Reckoning/Payload_3_The_Reckoning.ino
@@ -1,4 +1,6 @@
 #include <Wire.h>
+#include <SD.h>
+#include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
 #include <Adafruit_BMP085_U.h>
@@ -11,60 +13,15 @@ Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
 Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
 Adafruit_L3GD20_Unified       gyro  = Adafruit_L3GD20_Unified(20);
 
-void displaySensorDetails(void) {
-    sensor_t sensor;
-
-    accel.getSensor(&sensor);
-    Serial.println(F("----------- ACCELEROMETER ----------"));
-    Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
-    Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
-    Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
-    Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" m/s^2"));
-    Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" m/s^2"));
-    Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" m/s^2"));
-    Serial.println(F("------------------------------------"));
-    Serial.println(F(""));
-
-    gyro.getSensor(&sensor);
-    Serial.println(F("------------- GYROSCOPE -----------"));
-    Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
-    Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
-    Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
-    Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" rad/s"));
-    Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" rad/s"));
-    Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" rad/s"));
-    Serial.println(F("------------------------------------"));
-    Serial.println(F(""));
-
-    mag.getSensor(&sensor);
-    Serial.println(F("----------- MAGNETOMETER -----------"));
-    Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
-    Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
-    Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
-    Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" uT"));
-    Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" uT"));
-    Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" uT"));
-    Serial.println(F("------------------------------------"));
-    Serial.println(F(""));
-
-    bmp.getSensor(&sensor);
-    Serial.println(F("-------- PRESSURE/ALTITUDE ---------"));
-    Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
-    Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
-    Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
-    Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" hPa"));
-    Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" hPa"));
-    Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" hPa"));
-    Serial.println(F("------------------------------------"));
-    Serial.println(F(""));
-
-}
+// Used for storing the data we collect.
+File dataLog;
+char fileName[] = "log.txt";
 
 void setup(void) {
     Serial.begin(9600);
-    Serial.println(F("Adafruit 10DOF Tester")); Serial.println("");
-
+    pinMode(SS, OUTPUT);
     /* Initialise the sensors */
+
     if (!accel.begin()) {
         /* There was a problem detecting the ADXL345 ... check your connections */
         Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
@@ -86,55 +43,158 @@ void setup(void) {
         while (1);
     }
 
-    /* Display some basic information on this sensor */
-    displaySensorDetails();
+    if (!SD.begin(10)) {
+        Serial.println("Initialization of SD card failed!");
+        while (1);
+    }
+    
+    if (SD.exists(fileName)) {
+      SD.remove(fileName);
+    }
+
+
+    dataLog = SD.open( fileName , FILE_WRITE);
+
+    if (dataLog) {
+        Serial.println("Opened file correctly!");
+
+    } else {
+        Serial.println("Error opening file");
+    }
+
+    if (SD.exists(fileName)) {
+        Serial.println("file exists.");
+    } else {
+        Serial.println("file doesn't exist.");
+    }
+
 }
 
 void loop(void) {
     /* Get a new sensor event */
     sensors_event_t event;
 
+    // prints out: X Accelerometer data | Magnetometer data | gyroscopic data | pressure data Z
+    //                    X Y Z       |       X Y Z       |       X Y Z     | PRESSURE TEMPERATURE ALTITUDE Z
+
     /* Display the results (acceleration is measured in m/s^2) */
     accel.getEvent(&event);
-    Serial.print(F("ACCEL "));
-    Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
-    Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
-    Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  "); Serial.println("m/s^2 ");
+    //Serial.print(F("ACCEL "));
+    //Serial.print("X: ");
+    Serial.print(event.acceleration.x);
+    Serial.print(" ");
+    //Serial.print("Y: ");
+    Serial.print(event.acceleration.y);
+    Serial.print(" ");
+    //Serial.print("Z: ");
+    Serial.print(event.acceleration.z);
+    Serial.print(" | ");
+    //Serial.println("m/s^2 ");
+
+    //dataLog.print(F("ACCEL "));
+    //dataLog.print("X: ");
+    dataLog.print(event.acceleration.x);
+    dataLog.print(" ");
+    //dataLog.print("Y: ");
+    dataLog.print(event.acceleration.y);
+    dataLog.print(" ");
+    //dataLog.print("Z: ");
+    dataLog.print(event.acceleration.z);
+    dataLog.print(" | ");
+    //dataLog.println("m/s^2 ");
 
     /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
     mag.getEvent(&event);
-    Serial.print(F("MAG   "));
-    Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
-    Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
-    Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  "); Serial.println("uT");
+    //Serial.print(F("MAG   "));
+    //Serial.print("X: ");
+    Serial.print(event.magnetic.x);
+    Serial.print(" ");
+    //Serial.print("Y: ");
+    Serial.print(event.magnetic.y);
+    Serial.print(" ");
+    //Serial.print("Z: ");
+    Serial.print(event.magnetic.z);
+    Serial.print(" | ");
+    //Serial.println("uT");
+
+    //dataLog.print(F("MAG   "));
+    //dataLog.print("X: ");
+    dataLog.print(event.magnetic.x);
+    dataLog.print(" ");
+    //dataLog.print("Y: ");
+    dataLog.print(event.magnetic.y);
+    dataLog.print(" ");
+    //dataLog.print("Z: ");
+    dataLog.print(event.magnetic.z);
+    dataLog.print(" | ");
+    //dataLog.println("uT");
 
     /* Display the results (gyrocope values in rad/s) */
     gyro.getEvent(&event);
-    Serial.print(F("GYRO  "));
-    Serial.print("X: "); Serial.print(event.gyro.x); Serial.print("  ");
-    Serial.print("Y: "); Serial.print(event.gyro.y); Serial.print("  ");
-    Serial.print("Z: "); Serial.print(event.gyro.z); Serial.print("  "); Serial.println("rad/s ");
+    //Serial.print(F("GYRO  "));
+    //Serial.print("X: ");
+    Serial.print(event.gyro.x);
+    Serial.print(" ");
+    //Serial.print("Y: ");
+    Serial.print(event.gyro.y);
+    Serial.print(" ");
+    //Serial.print("Z: ");
+    Serial.print(event.gyro.z);
+    Serial.print(" | ");
+    //Serial.println("rad/s ");
+
+    //dataLog.print(F("GYRO  "));
+    //dataLog.print("X: ");
+    dataLog.print(event.gyro.x);
+    dataLog.print(" ");
+    //dataLog.print("Y: ");
+    dataLog.print(event.gyro.y);
+    dataLog.print(" ");
+    //dataLog.print("Z: ");
+    dataLog.print(event.gyro.z);
+    dataLog.print(" | ");
+    //dataLog.println("rad/s ");
 
     /* Display the pressure sensor results (barometric pressure is measure in hPa) */
     bmp.getEvent(&event);
     if (event.pressure) {
         /* Display atmospheric pressure in hPa */
-        Serial.print(F("PRESS "));
+        //Serial.print(F("PRESS "));
         Serial.print(event.pressure);
-        Serial.print(F(" hPa, "));
+        Serial.print(F(" "));
+
+        //dataLog.print(F("PRESS "));
+        dataLog.print(event.pressure);
+        dataLog.print(F(" "));
+
         /* Display ambient temperature in C */
         float temperature;
         bmp.getTemperature(&temperature);
         Serial.print(temperature);
-        Serial.print(F(" C, "));
+        Serial.print(F(" "));
+
+        dataLog.print(temperature);
+        dataLog.print(F(" "));
+
         /* Then convert the atmospheric pressure, SLP and temp to altitude    */
         /* Update this next line with the current SLP for better results      */
         float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
         Serial.print(bmp.pressureToAltitude(seaLevelPressure,
                                             event.pressure,
                                             temperature));
-        Serial.println(F(" m"));
+        Serial.println(F(" "));
+
+        dataLog.print(bmp.pressureToAltitude(seaLevelPressure,
+                                             event.pressure,
+                                             temperature));
+        dataLog.println(F(" "));
     }
 
-    Serial.println(F(""));
+    //Serial.println(F(""));
+    //dataLog.println(F(""));
+    dataLog.flush();
 }
+
+
+
+
